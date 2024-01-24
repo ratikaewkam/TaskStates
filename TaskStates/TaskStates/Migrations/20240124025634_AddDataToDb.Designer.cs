0958de90c57b97bfc527c964aeb70c5fe3bde37e
Rati Kaewkam <ratikaewkam.rk@gmail.com>
2024-01-24 10:00:05 +0700

    Update Migration

--- TaskStates/TaskStates/Migrations/20240124025634_AddDataToDb.Designer.cs
+++ TaskStates/TaskStates/Migrations/20240124025634_AddDataToDb.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using TaskStates.Data;
 
@@ -11,9 +12,11 @@ using TaskStates.Data;
 namespace TaskStates.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240124025634_AddDataToDb")]
+    partial class AddDataToDb
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
