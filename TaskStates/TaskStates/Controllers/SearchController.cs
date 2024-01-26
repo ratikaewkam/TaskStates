6018c271b1a14ea0da8adc08f83f070713fddce1
Rati Kaewkam <ratikaewkam.rk@gmail.com>
2024-01-26 13:36:40 +0700

    Update SearchController.cs

--- TaskStates/TaskStates/Controllers/SearchController.cs
+++ TaskStates/TaskStates/Controllers/SearchController.cs
@@ -1,12 +1,21 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using TaskStates.Data;
 
 namespace TaskStates.Controllers
 {
     public class SearchController : Controller
     {
-        public IActionResult Index()
+        private readonly ApplicationDbContext _db;
+
+        public SearchController(ApplicationDbContext db)
         {
-            return View();
+            _db = db;
+        }
+
+        public IActionResult Index(string title, string room)
+        {
+            var items = _db.Tasks.Where(x => x.TaskName.Contains(title) && x.Room.Contains(room)).ToList();
+            return View(items);
         }
     }
 }
