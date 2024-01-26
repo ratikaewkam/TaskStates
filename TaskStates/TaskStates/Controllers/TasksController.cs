9fdd27a296d7a184d9a2172809f1163e2d48de53
Rati Kaewkam <ratikaewkam.rk@gmail.com>
2024-01-26 16:30:00 +0700

    Update TasksController.cs

--- TaskStates/TaskStates/Controllers/TasksController.cs
+++ TaskStates/TaskStates/Controllers/TasksController.cs
@@ -4,7 +4,17 @@ namespace TaskStates.Controllers
 {
     public class TasksController : Controller
     {
-        public IActionResult Index()
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        public IActionResult Edit() 
+        {
+            return View();
+        }
+
+        public IActionResult Delete()
         {
             return View();
         }
